11a52b3016fdae444d9b23619ec6ce998d9fb84e
Hamza Hajji <hmzhjji@gmail.com>
2017-11-04 23:10:17 +0000

    add animation to article results

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+import {CSSTransitionGroup} from 'react-transition-group';
 
 export default ({
   articles
 }) => {
+  const renderArticles = (article, idx) => {
+    return (
+      <ListGroupItem key={idx} style={{transitionDelay: `${idx * 50}ms`}}>
+        <ListGroupItemHeading>
+          <a target="_blank" href={`https://wikipedia.org/wiki/${article.title}`}>
+            {article.title}
+          </a>
+        </ListGroupItemHeading>
+        <ListGroupItemText dangerouslySetInnerHTML={{__html: article.snippet}}>
+        </ListGroupItemText>
+      </ListGroupItem>
+    );
+  }
   return (
     <div className="col-md-8 col-10 mx-auto">
       <ListGroup className="results">
-        {articles.map((article, idx) => {
-          return (
-            <ListGroupItem>
-              <ListGroupItemHeading>
-                <a target="_blank" href={`https://wikipedia.org/wiki/${article.title}`}>
-                  {article.title}
-                </a>
-                </ListGroupItemHeading>
-                <ListGroupItemText dangerouslySetInnerHTML={{__html: article.snippet}}>
-                </ListGroupItemText>
-              </ListGroupItem>
-            );
-          })}
-        </ListGroup>
+        <CSSTransitionGroup
+          transitionName="article"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+          {articles.map(renderArticles)}
+      </CSSTransitionGroup>
+      </ListGroup>
     </div>
   );
 };
